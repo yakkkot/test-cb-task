90ebf705610345c5d2e962ce1768dd35a8567cf8
yakkkot <k0tjeee777@gmail.com>
2023-02-16 23:08:37 +0200

    All task.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ mainBlock.appendChild(button);
 
 const ref = document.createElement("a");
 ref.href = 'https://docs.google.com/spreadsheets/d/1hGyZbLlrAbVbWfe4yHd2tUM8f3e16D_iFqhYKBpW0Ls/edit#gid=0';
-ref.target = '_blank';
+ref.setAttribute('target', '_blank');
 ref.classList.add("ref");
 ref.innerText = "Open Google Spreadsheet";
 mainBlock.appendChild(ref);
@@ -22,7 +22,7 @@ async function buttonClick() {
         const columnHeaders = Array.from(document.querySelectorAll("grid-column-header"));
 
         if (allRows.length === 0 || columnHeaders.length === 0) {
-            throw new Error('The page does not have a table.');
+            window.alert('The page does not have a table.');
         }
 
         const res = [];
@@ -58,44 +58,9 @@ async function buttonClick() {
         })
         const dataJSON = await data.json()
         console.log(dataJSON);
-    }
-    catch (e){
+    } catch (e){
         console.log(e);
         window.alert(e);
     }
 }
-
-
-
-
-// allRows.forEach(row => {
-//     const resultFromRow= Array.from(row.childNodes).reduce((acc, value,index) => {
-//         const i = index.toString();
-//         if(value.textContent.trim()) {
-//             acc[i] = value.textContent;
-//         }
-//         return acc;
-//     }, {});
-//     res.push(resultFromRow);
-// });
-// console.log(res);
-//
-// fetch("https://sheet.best/api/sheets/b6b47b3c-c01f-4f7b-a3f9-2ad49ff7dd2f", {
-//     method: "POST",
-//     mode: "cors",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(res),
-// })
-//     .then((r) => r.json())
-//     .then((data) => {
-//         // The response comes here
-//         console.log(data);
-//     })
-//     .catch((error) => {
-//         // Errors are reported there
-//         console.log(error);
-//     });
-
 
